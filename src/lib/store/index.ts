ab5414458a127c543dbbfd6f0885265b74f49ac1
nazifbara <nazifbarassounon@gmail.com>
2022-12-30 09:30:51 +0100

    remove jobs store

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,16 +1,4 @@
-import type { Job } from '$lib/types';
 import { writable } from 'svelte/store';
-
-const createJobs = () => {
-	const { subscribe, set } = writable<Job[]>([]);
-
-	return {
-		subscribe,
-		set
-	};
-};
-
-export const jobs = createJobs();
 
 export const currentTheme = (() => {
 	const { subscribe, update, set } = writable<'light' | 'dark'>('light');
